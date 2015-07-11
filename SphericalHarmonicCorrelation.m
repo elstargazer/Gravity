3900bca4067e9d606b0623b4d72f9151ba91dd50
AntonErmakov <anton.i.ermakov@gmail.com>
2015-07-10 20:19:16 -0400

    added newlines

--- SphericalHarmonicCorrelation.m
+++ SphericalHarmonicCorrelation.m
@@ -4,17 +4,13 @@ MaxDegree=min(lmcosi1(end,1),lmcosi2(end,1));
 
 Corr=zeros(1,MaxDegree);
 
-for i=1:MaxDegree
+for i=1:MaxDegree+1
     
     i_start=(i-1)*(i)/2+1;
     i_end=(i)*(i+1)/2;   
-    
-    
+        
     Corr(i)=sum(lmcosi1(i_start:i_end,3).*lmcosi2(i_start:i_end,3)+lmcosi1(i_start:i_end,4).*lmcosi2(i_start:i_end,4))./...
         sqrt(sum(lmcosi1(i_start:i_end,3).^2+lmcosi1(i_start:i_end,4).^2)*sum(lmcosi2(i_start:i_end,3).^2+lmcosi2(i_start:i_end,4).^2));
-    
-    
-    
 end
 
 % figure;
