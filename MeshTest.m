9ff00f3acc96c4d26ee3677ddb0129b5793c3503
AntonErmakov <anton.i.ermakov@gmail.com>
2015-07-29 00:31:58 -0400

    added ellipsoid example

--- MeshTest.m
+++ MeshTest.m
@@ -5,33 +5,27 @@ shape_filename='SHAPE_SPC150716_256.bds';
 [~,shapename,~] = fileparts(shape_filename) ;
 shape_full_filename = [shape_folder shape_filename];
 
-filename_mesh = '../CeresFE/FE/mesh.inp';
-nrow = 10;
-Shape2Mesh_cubesphere(shape_full_filename,filename_mesh,nrow);
+filename_mesh = '../CeresFE/FE/mesh_gibbon.inp';
+
+r_sphe  = 470000;
+r_core  = 390000;
+n_core  = 30;
+n_sphe  = 30;
+
+% Shape2Mesh_gibbon(r_sphe, r_core, n_core, n_sphe, ...
+%     shape_full_filename, filename_mesh);
+
+RandomShape2Mesh_gibbon(r_sphe, r_core, n_core, n_sphe, ...
+    filename_mesh)
+
+
+%% Generate Jacobi ellipsoid
+% T   = 4.6;
+% rho = 2169;
+% [fh,fval] = HydrostaticStateExact3Ax(r_sphe,T,rho,0.05,0.025);
+% [a,b,c] = fr2abc(r_sphe,fh(1),fh(2));
+% Ellipsoid2Mesh_gibbon([a b c], r_core, n_core, n_sphe, filename_mesh)
+
+% nrow = 10;
+% Shape2Mesh_cubesphere(shape_full_filename,filename_mesh,nrow);
  
-% NTess = 0;
-% 
-% TR=IcosahedronMesh;
-% TR_2=SubdivideSphericalMesh(TR,NTess); 
-% 
-% FV=TR_2.Triangulation;
-% x=TR_2.X(:,1);
-% y=TR_2.X(:,2);
-% z=TR_2.X(:,3);
-% 
-% figure; hold on;
-% axis equal;
-% axis off;
-% 
-% trisurf(TR_2);
-% for i=1:numel(x)
-%    
-%     plot3(x(i),y(i),z(i),'o','MarkerSize',20);
-%     text(1.1*x(i),1.1*y(i),1.1*z(i),num2str(i),'FontSize',20);
-%     
-% end
-% 
-% filename_mesh = '../CeresFE/FE/mesh.geo';
-% Shape2Mesh_gmsh(x,y,z,FV,filename_mesh);
- 
-% 
