de090c7555d7cb78f3f3ae9633d6fc0a50959168
AntonErmakov <anton.i.ermakov@gmail.com>
2015-09-24 23:36:29 -0400

    added radiul gravity difference

--- CompareGravityFromShape.m
+++ CompareGravityFromShape.m
@@ -39,21 +39,27 @@ aref = ell_min(3);
 bref = ell_min(2);
 cref = ell_min(1);
 
-[xref,yref,zref]=TriEllRadVec(lati,loni,aref,bref,cref,'xyz');
+[xref,yref,zref]=TriEllRadVec(lati,loni,aref,aref,cref,'xyz');
 
 %% Compute gravity 
 s=size(FV1);
 rho=ones(1,s(1))*rho;
 
 [ax1,ay1,az1]=GravityAccelerationTriDen(x1,y1,z1,FV1,xref,yref,zref,rho);
-[ax2,ay2,az2]=GravityAccelerationTriDen(x2,y2,z2,FV2,xref,yref,zref,rho);
+[ax1_up,ax1_east,ax1_north]=GravityComponents(ax1,ay1,az1,xref,yref,zref,aref,cref);
 
-dg = sqrt((ax1-ax2).^2+(ay1-ay2).^2+(az1-az2).^2)*1e5;
+[ax2,ay2,az2]=GravityAccelerationTriDen(x2,y2,z2,FV2,xref,yref,zref,rho);
+[ax2_up,ax2_east,ax2_north]=GravityComponents(ax2,ay2,az2,xref,yref,zref,aref,cref);
+
+ dg = sqrt((ax1-ax2).^2+(ay1-ay2).^2+(az1-az2).^2)*1e5;
+ dg_up = ax1_up - ax2_up;
 
 AGUaxes;
-pcolorm(lati*180/pi,loni*180/pi,dg); 
+pcolorm(lati*180/pi,loni*180/pi,dg_up*1e5); 
 cbar = colorbar('FontSize',20);
-ylabel(cbar,'Acceleration difference vector [mGal]','FontSize',20);
+ylabel(cbar,'Acceleration difference [mGal]','FontSize',20);
+
+
 
 
 
