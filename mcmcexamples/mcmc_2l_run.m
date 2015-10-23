87198c4aa7861f244493e83c64416095f4bd91aa
AntonErmakov <anton.i.ermakov@gmail.com>
2015-10-23 14:05:18 -0400

    does not get stuck now, added while loop

--- mcmcexamples/mcmc_2l_run.m
+++ mcmcexamples/mcmc_2l_run.m
@@ -1,49 +1,35 @@
-function [param]=mcmc_2l_run(N, s, data, param0, cost_fun,step_param,T,r2,M)
+function [param]=mcmc_2l_run(N,s,data,param0,cost_fun,step_param,T,r2,M)
 
+% initialize parameter vectors
 param=zeros(N,2);
-
 param(1,:)=param0;
 
+% compute initial cost
 cost_c=cost_fun(param(1,:),data,T,r2,M);
 
-progressbar(0);
+% cost_c -- old cost
+% cost_p -- new (proposed) cost
 
 for i=1:N
     
-    param_p = step_param(param(i,:),s);
-    
-    
-    r1_p=param_p(1);
-    rho1_p=param_p(2);
-   
-    rho2=-(3*M-4*pi*(r1_p.^3).*rho1_p)./...
-    (4*pi*(r1_p.^3)-4*pi*(r2^3));
-
-    cond = ((rho2>0) && (rho1_p>0) && (r1_p>0));
-    
-    if cond
-        cost_p = cost_fun(param_p,data,T,r2,M);
-    else
-        param_p = param0;
-        cost_p = cost_fun(param_p,data,T,r2,M);
-        1
+    cond = 0;
+    while (~cond)
+        param_p = step_param(param(i,:),s);
+        r1_p=param_p(1);
+        rho1_p=param_p(2);
+        rho2=-(3*M-4*pi*(r1_p.^3)*rho1_p)/...
+            (4*pi*(r1_p^3)-4*pi*(r2^3));
+        cond = ((rho2>0) && (rho1_p>0) && (r1_p>0));
     end
     
+    cost_p = cost_fun(param_p,data,T,r2,M);
     metric=exp(- (cost_p) + (cost_c));
-     
-    if (rand < metric) && ~isnan(cost_p) && ~isnan(metric)
-        
-        param(i+1,:) = param_p;    
-        cost_c=cost_p;
-        
+    
+    if (rand < metric) && (~isnan(cost_p)) && (~isnan(metric))
+        param(i+1,:) = param_p;
+        cost_c       = cost_p;
     else
-        param(i+1,:) = param(i,:);  
-
+        param(i+1,:) = param(i,:);
     end
-    
-    progressbar(i/N);
-    
 end
 
-
- progressbar(1);
