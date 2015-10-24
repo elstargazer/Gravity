38408965c079878038500912163b1935a3b5f104
AntonErmakov <anton.i.ermakov@gmail.com>
2015-10-24 17:35:44 -0400

    added J2 cost

--- mcmcexamples/cost_fun_flat.m
+++ mcmcexamples/cost_fun_flat.m
@@ -6,14 +6,15 @@ rho1=param(2);
 rho2=-(3*M-4*pi*(r1.^3).*rho1)./(4*pi*(r1.^3)-4*pi*(r2^3));
 
 % f=HydrostaticStateExact2l(r2,r1,T,rho2,rho1,0.1,0.1);
-f=HydrostaticState2LayerAn(r1,r2,T,rho1,rho2,0.1,0.1,6);
+f=HydrostaticState2LayerAn(r1,r2,T,rho1,rho2,0.1,0.1,2);
 
 f_obs=data(1);
 J2_obs=data(2);
 
-% J2=RadFlat2J2(r2,r1,f(2),f(1),rho2,rho1,500000);
-cost=((f(2)-f_obs).^2)./(0.005.^2);
+J2=RadFlat2J2(r2,r1,f(2),f(1),rho2,rho1,476000);
 
+cost=((f(2) - f_obs).^2)./(0.004^2) + ...
+     ((J2  - J2_obs).^2)./(25e-5^2);
 
 
 
